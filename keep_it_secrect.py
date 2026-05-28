2eb65da62e8d5eaff37ebc91196d8e9bb0cf9a46
Hridita-11 <hridita.nz@gmail.com>
2026-05-28 18:20:21 +1200

    Add files via upload

---
+++ keep_it_secrect.py
@@ -0,0 +1,11 @@
+class myClass:
+    __privetvar = 27;
+    def __privMeth(self):
+        print("I am inside class myClass")
+    def hello(Self):
+        print("Privet variable value:",myClass.__privetvar)
+foo = myClass()
+foo.hello()
+foo.__privMeth
+
+
